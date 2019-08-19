2ce53643277d1d7708ee26d9668b3d933f076868
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-19 16:02:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
  
  for (var i=0; i < musicians.length; i++) {
    
-   var msg = `${musicians[i]} plays the `
+   var msg = `${musicians[i]} plays ${instruments[i]`
  }
  
   
